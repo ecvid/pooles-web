8126e2ba5d2ecd296a6cdbd129b6ada2463d686c
David Villalonga <davidvillalonga1975@gmail.com>
2022-11-21 19:45:02 +0100

    Licencia OK. Eliminar, Actualizar y Añadir

--- routes/licencias/licencias.mjs
+++ routes/licencias/licencias.mjs
@@ -24,9 +24,37 @@ router.get('/eliminar', isLoggedIn, async (req, res) => {
 
 router.post('/eliminar', isLoggedIn, async (req, res) => {
 
-    console.log(req.body.licencia)
-
-    //await crud.remove(req.body.licencia)
+    await crud.remove(req.body.licencia)
 
     res.redirect('/licencias')
 })
+
+router.get('/anyadir', isLoggedIn, (req, res) => {
+
+    res.render('licencias/anyadir', {
+        title: "GESTIÓN RRHH POOLES " + ANY
+    })
+})
+
+router.post('/anyadir', isLoggedIn, async (req, res) => {
+
+    await crud.insert(req.body.licencia)
+
+    res.redirect('/licencias')
+})
+
+router.get('/actualizar', isLoggedIn, (req, res) => {
+
+    res.render('licencias/actualizar', {
+        title: "GESTIÓN RRHH POOLES " + ANY,
+        licencia: req.query.licencia
+    })
+})
+
+router.post('/actualizar', isLoggedIn, async (req, res) => {
+
+    await crud.update(req.body.licencia, req.body.nuevaLicencia)
+
+    res.redirect('/licencias')
+})
+
